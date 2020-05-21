d62d7938a2dde851f8e0acc5934ed52b5b68f9a1
ashankk2 <ashankk2@illinois.edu>
2020-05-21 01:30:24 -0400

    Added function to convert pdf to string and get unformatted list of classes

--- parser.py
+++ parser.py
@@ -1,69 +1,55 @@
-from pdfminer.pdfinterp import PDFResourceManager,PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from io import BytesIO
+import pdftotext
+import unicodedata
 import re
-from dataclasses import dataclass
-from minor import minor
+import subprocess
 
-# Function takes the path to a pdf file and returns a string
-def pdf_to_string(path):
-    manager = PDFResourceManager()
-    retstr = BytesIO()
-    layout = LAParams(all_texts=True)
-    device = TextConverter(manager, retstr, laparams=layout)
-    filepath = open(path, 'rb')
-    interpreter = PDFPageInterpreter(manager, device)
-    for page in PDFPage.get_pages(filepath, check_extractable=True):
-        interpreter.process_page(page)
-    text = retstr.getvalue()
-    filepath.close()
-    device.close()
-    retstr.close()
-    text = text.decode('utf-8')
-    return text
+#TODO: handle cases in which the passed file is not a pdf or is not an actual dars report
+# Function takes the path to a pdf and then converts it to a string
+def convert_pdf_text(path):
+    with open(path, 'rb') as f:
+        pdf = pdftotext.PDF(f)
 
-# Function takes a string representation of the transcript and returns a list of strings that are the classes a student has taken or is taking
-def unformatted_string_to_classes(text):
-    sat_act_credit = act_sat_credit(text)
-    print (sat_act_credit)
+    whole_report = str()
 
-# Function takes a string representation of the transcript and returns a list of tuples that are classes a student has recieved credit for from the the act/sat
-# Function is a helper function for unformatted_string_to_classes
-# Currently the only class one can recieve credit for from the act/sat is rhet 105 it seems
-# TODO: Obtain a transcript where a student does not have credit for RHET 105 and adapt this function for that case
-# It could be possible to search the entire string for 'RHET' and if it doesn't exist return an empty list
-def act_sat_credit(text):
+    for page in pdf:
+        whole_report += page
 
-    # Get the portion of the string that contains the credit earned from sat/act
-    act_sat_unformatted = text[text.rfind('TRANSFER CREDIT ACCEPTED BY INSTITUTION'):text.rfind("Advanced Placement Tests")]
+    # If whole_report is empty then a possible case is that the dars 
+    # report can't be converted and must be converted using ocrmypdf
+    if not whole_report:
+        subprocess.call(['bash', 'converter.sh', path])
     
-    # Convert that string into a list of all the lines in the string
-    unformatted_string_list = act_sat_unformatted.split('\n')
-    
-    # Remove the empty strings in the list
-    unformatted_string_list = [s for s in unformatted_string_list if s]
+    with open(path, 'rb') as f:
+        pdf = pdftotext.PDF(f)
 
-    courses_and_course_nums = []
-    for s in unformatted_string_list:
-        # Removes string in list that are not between length of 3 and 4 (Course codes are of either length 3 or 4 and course numbers are of length 3) 
-        # Need to verify this though
-        if (len(s) <= 4 and len(s) >= 3): 
-            # Removes string that contain the substring '.' or 'GPA'
-            if not ('.' in s) and not ('GPA' in s):
-                courses_and_course_nums.append(s)
+    whole_report = str()
 
-    # Set courses equal to the first half of courses_and_course_nums
-    courses = courses_and_course_nums[:len(courses_and_course_nums)//2]
-    # Set course_nums equal to the second half of courses_and_course_nums
-    course_nums = courses_and_course_nums[len(courses_and_course_nums)//2:]
+    for page in pdf:
+        whole_report += page
 
-    # Create a list of tuples from the two lists
-    course_and_nums_tuples = list(zip(courses, course_nums))
-    
-    return course_and_nums_tuples
+    return whole_report
 
+# Function takes a dars report in string format and returns a list of unformatted strings representing that courses a student has taken or is taking
+def get_courses_from_text(text):
+    # Concatenates text to subbstring in text between last found occurences of 'SUMMARY OF COURSES TAKEN' and 'My Audit'
+    text = text[text.rfind('SUMMARY OF COURSES TAKEN'):text.rfind('My Audit')]
 
-unformatted_text = pdf_to_string('test.pdf') 
-unformatted_string_to_classes(unformatted_text)
+    # Creats a list of all the lines in text
+    lines = text.split('\n')
+
+    # Uses regex to remove all extra spaces as well as leading spaces from a string. Example " Quick    Brown    Fox" -> "Quick Brown Fox"
+    lines = [re.sub(' +', ' ', s.lstrip()) for s in lines]
+
+    # Removes strings whose first 4 characters are not two letters followed by two digits
+    # The list of courses a student has taken is in the form of [Term][Year] followed by the course and more information
+    # [Term] and [Year] are represented by 2 uppercase letters and two digits respectively 
+    # Therefore strings in lines that do not follow this format are not lines containing a course a student has taken and we can throw them out
+    courses = [s for s in lines if re.match('[A-Z].*[A-Z][1-9][0-9]', s[0:4])]
+
+    return courses
+
+text = convert_pdf_text('roe3.pdf')
+courses = get_courses_from_text(text)
+print (courses)
+
+ 
