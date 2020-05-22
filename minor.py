ab49bbaa7a886adf9b2c97f88f367426197ec9e6
ameyagh <ameyapg2@illinois.edu>
2020-05-21 20:05:54 -0500

    abstraction to minor class

--- minor.py
+++ minor.py
@@ -1,10 +1,9 @@
-from dataclasses import dataclass
 from typing import List
 import collections
 from group import group
+from abc import ABC, abstractmethod
 
-@dataclass
-class minor:
+class minor(ABC):
     def __init__(self, name_ : str, required_courses_ : list, required_groups_ : list, total_credits_ : int):
         self.name = name_
         self.required_courses = required_courses_
@@ -17,11 +16,10 @@ class minor:
     def __repr__(self):
         return (f'{self.name}, {self.total_credits}, {self.required_courses}, {self.required_groups}')
 
-# cs_group = group(2, 3, ['CS 233', 'CS 241', 'CS 357', 'CS 374'])
-# comp_sci_minor = minor('Computer Science', ['CS 125', 'CS 173', 'CS 225'], [cs_group], 20)
-# print(comp_sci_minor)
+""" types of minors
+- regular minor easy to parse into groups
+- area choice minor (given areas choose classes from the areas)
 
-# computer_science_minor = minor('Computer Science', ['CS 125', 'CS 173', 'CS 225'], 11, ['CS 233', 'CS 241', 'CS 357', 'CS 374', 'CS 410'], 9, [['CS 125', 'CS 173', 'CS 225']])
-# # print(computer_science_minor)
-# test = ['CS 125', 'CS 225']
-# print (computer_science_minor.valid_required_classes_subset(test))
+
+
+"""
