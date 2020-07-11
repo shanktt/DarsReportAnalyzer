f5b198c46cd857aee52be1fd1493ed41ca8c5e0e
AshankKumar <ashankkumar01@gmail.com>
2020-07-11 17:50:25 -0400

    Added comments to new methods in group.py

--- group.py
+++ group.py
@@ -15,8 +15,10 @@ class group:
     def get_courses(self):
         return [c[0] for c in self.courses]
 
+    # Converts the list of tuples into a flat list: [('CS 125', 'ECE 220'), ('CS 173', 'MATH 213')] -> ['CS 125', 'ECE 220', 'CS 173', 'MATH 213']
     def get_repl_courses_as_flat_list(self):
         return [course for courses_tuple in self.repl_courses for course in courses_tuple]
 
+    # Converts the list of tuples into a dictionary where the first element in the tuple is the key the and rest of the elements in the tuple are the value (in the form of a list of strings)
     def convert_repl_courses_into_dict(self):
         return dict([(repl_tuple[0], list(repl_tuple[1:])) for repl_tuple in self.repl_courses])
